0b7976215554db0681f05a32b6842d90b1b6b105
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-01-31 22:59:48 +0100

    add basic buttons 1/2

--- src/inputs/Button/Button.stories.tsx
+++ src/inputs/Button/Button.stories.tsx
@@ -1,13 +1,32 @@
-// Button.stories.tsx
 import { Meta, Story } from "@storybook/preact";
-import { h } from "preact";
-import { Button } from "./Button";
+import Button from "./Button";
 import * as React from "preact/compat";
+import { h } from "preact";
 
 export default {
   component: Button,
 } as Meta;
 
-export const Primary: Story = () => <Button>Click me</Button>;
+export const Primary = () => (
+  <Button variant="primary" onClick={() => console.log("clicked")}>
+    Primary
+  </Button>
+);
 
-Primary.args = {};
+export const Secondary = () => (
+  <Button variant="secondary" onClick={() => console.log("clicked")}>
+    Secondary
+  </Button>
+);
+
+export const Loading = () => (
+  <Button isLoading onClick={() => console.log("clicked")}>
+    Loading
+  </Button>
+);
+
+export const Disabled = () => (
+  <Button disabled onClick={() => console.log("clicked")}>
+    Disabled
+  </Button>
+);
--- src/inputs/Button/Button.tsx
+++ src/inputs/Button/Button.tsx
@@ -1,8 +1,36 @@
-import { PropsWithChildren } from "preact/compat";
-import { h } from "preact";
+import React from 'react'
+import { PropsWithChildren } from 'react'
+import { h } from 'preact'
+import styles from './Button.module.css'
+import ButtonStories from './Button.stories'
 
-interface ButtonProps {}
+interface ButtonProps {
+	type?: 'button' | 'submit'
+	disabled?: boolean
+	icon?: React.ReactElement
+	isLoading?: boolean
+	onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+	variant: 'primary' | 'secondary'
+}
 
-export const Button = (props: PropsWithChildren<ButtonProps>) => {
-  return <button>{props.children}</button>;
-};
+const Button: React.FC<PropsWithChildren<ButtonProps>> = ({
+	type = 'button',
+	disabled = false,
+	icon,
+	isLoading = false,
+	onClick,
+	variant,
+	children,
+}) => {
+	return (
+		<button
+			className={`${styles.buttons}, {variant === "primary" ? ${styles.primary} : ${styles.secondary}}`}
+			type={type}
+			disabled={disabled}
+			onClick={onClick}>
+			{children}
+		</button>
+	)
+}
+
+export default Button
