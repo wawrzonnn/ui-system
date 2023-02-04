5c56529898396d7be230d430dc27dfd0475d2ebf
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-02-04 13:29:20 +0100

    Task 3 Resolved

---
+++ src/navigation/Link/Link.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { h } from 'preact'
+import styles from './Link.module.css'
+
+interface LinkProps {
+	to: string
+	id?: string
+	target?: '_blank' | '_self' | '_parent' | '_top'
+	disabled?: boolean
+	onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void
+	color: string
+}
+
+const Link: React.FC<LinkProps> = ({
+	to = 'www.google.pl',
+	id = 'google',
+	target = '_blank',
+	disabled = false,
+	onClick,
+	color = 'tomato',
+	children,
+}) => {
+	const globalLinkClass = styles.linkGlobal
+	const optionClass = disabled === false ? styles.linkEnabled : styles.linkDisabled
+	const allClasses = [globalLinkClass, optionClass].join(' ')
+	return (
+		<a className={allClasses} id={id} target={target} disabled={disabled} onClick={onClick}>
+			{children}
+		</a>
+	)
+}
+
+export default Link
