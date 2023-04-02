9b309f3654a99fa5b69dd106aebf9aa5568e97c8
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-04-02 20:52:01 +0200

    feat: install Husky for pre-commit hooks

--- src/inputs/Switch/Switch.test.tsx
+++ src/inputs/Switch/Switch.test.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Switch from './Switch';
 
-
-
 describe('Switch', () => {
-it('should call onChange when clicked', () => {
-const onChange = jest.fn();
-const { getByLabelText } = render(<Switch label="label" onChange={onChange} />);
-const switchEl = getByLabelText('label');
-fireEvent.click(switchEl);
-expect(onChange).toHaveBeenCalled();
-});
+  it('should call onChange when clicked', () => {
+    const onChange = jest.fn();
+    const { getByLabelText } = render(
+      <Switch label="label" onChange={onChange} id="switch-1" />
+    );
+    const switchEl = getByLabelText('label');
+    fireEvent.click(switchEl);
+    expect(onChange).toHaveBeenCalled();
+  });
 
-it('should not call onChange when disabled', () => {
-const onChange = jest.fn();
-const { getByLabelText } = render(<Switch label="label" disabled onChange={onChange} />);
-const switchEl = getByLabelText('label');
-fireEvent.click(switchEl);
-expect(onChange).not.toHaveBeenCalled();
-});
+  it('should not call onChange when disabled', () => {
+    const onChange = jest.fn();
+    const { getByLabelText } = render(
+      <Switch label="label" disabled onChange={onChange} id="switch-1" />
+    );
+    const switchEl = getByLabelText('label');
+    fireEvent.click(switchEl);
+    expect(onChange).not.toHaveBeenCalled();
+  });
 
-it('should change checked state when clicked', () => {
-const { getByLabelText } = render(<Switch label="label" />);
-const switchEl = getByLabelText('label') as HTMLInputElement
-fireEvent.click(switchEl);
-expect(switchEl.checked).toBe(true);
-fireEvent.click(switchEl);
-expect(switchEl.checked).toBe(false);
-});
+  it('should change checked state when clicked', () => {
+    const { getByLabelText } = render(<Switch label="label" id="switch-1" />);
+    const switchEl = getByLabelText('label') as HTMLInputElement;
+    fireEvent.click(switchEl);
+    expect(switchEl.checked).toBe(true);
+    fireEvent.click(switchEl);
+    expect(switchEl.checked).toBe(false);
+  });
 });
