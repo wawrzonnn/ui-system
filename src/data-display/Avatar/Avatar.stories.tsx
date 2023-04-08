2be05df7fd62773c021cd4081fa1062fb88cf5ef
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-04-08 07:23:17 +0200

    feat(avatar): add avatar

--- src/data-display/Avatar/Avatar.stories.tsx
+++ src/data-display/Avatar/Avatar.stories.tsx
@@ -1,25 +1,30 @@
 import { Meta, Story } from "@storybook/react";
 import { Avatar } from "./Avatar";
 import React from "react";
-import { AvatarTemplate }  from  '../../assets/images/AvatarTemplate.jpg';
+import AvatarTemplate from "../../assets/images/AvatarTemplate.jpg";
 
 export default {
   component: Avatar,
   argTypes: {
-    // icon: {
-    //   options: {
-    //     none: null,
-    //     Settings: <Settings />,
-    //   },
-    // },
+    src: {
+      control: {
+        type: "radio",
+        labels: {
+          none: "None",
+          image: "AvatarTemplate",
+        },
+      },
+    },
   },
 } as Meta;
 
 export const Default: Story = (args) => (
-  <Avatar src={AvatarTemplate} alt={""} {...args}  >fdggfd
+  <Avatar src={AvatarTemplate} alt={"handsome boy"} {...args}>
+    fdggfd
   </Avatar>
 );
 Default.args = {
-  size: 'big',
-  alt: 'sumick'
+  size: "big",
+  src: AvatarTemplate,
+  alt: "sumick",
 };
--- src/data-display/Avatar/Avatar.tsx
+++ src/data-display/Avatar/Avatar.tsx
@@ -4,19 +4,28 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 interface AvatarProps {
-src: string;
-alt: string;
-size?: 'big' | 'medium' | 'small'
+  src: string;
+  alt: string;
+  size?: "big" | "medium" | "small";
 }
 
-export const Avatar = ({ size = 'big', src, alt
+export const Avatar = ({
+  size = "big",
+  src,
+  alt,
 }: PropsWithChildren<AvatarProps>) => {
   const avatarClasses = cx({
     avatar: true,
     [size]: true,
+    avatarContainer: !src,
+    avatarPlaceholder: !src,
   });
 
-  return (
-   <img className={avatarClasses} src={src} alt={alt} />
+  return src ? (
+    <img className={avatarClasses} src={src} alt={alt} />
+  ) : (
+    <div className={avatarClasses}>
+      <span>A</span>
+    </div>
   );
 };
