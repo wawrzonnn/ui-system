e50d8767e72697ce8e8d6a9fb17a3270c28f27cb
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-04-26 13:57:18 +0200

    feat: add card component

--- src/surface-components/Card.stories.tsx
+++ src/surface-components/Card.stories.tsx
@@ -1,41 +1,52 @@
-/* eslint-disable react/no-children-prop */
-import React from "react";
-import { Meta, Story } from "@storybook/react";
-import { Card, CardImage, CardContent } from "./Card";
-import { Button } from "../inputs";
-import AvatarTemplate from "../assets/images/CardAvatar.png";
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import { Card, CardImage, CardContent } from './Card'
+import { Button } from '../inputs'
+import AvatarTemplate from '../assets/images/CardAvatar.png'
+import styles from './Card.module.css'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 export default {
-  component: Card,
-} as Meta;
+	component: Card,
+} as Meta
 
-export const Default: Story = (args) => (
-  <Card {...args}>
-    <CardImage url={AvatarTemplate} alt={"Default Avatar"}></CardImage>
-    <CardContent>
-      <h1>Card name</h1>
-      <span>
-        Card description goes here and it can be as long as it needs and so on.
-      </span>
-      <Button
-        onClick={() => {
-          console.log("Hello");
-        }}
-        variant={"primary"}
-      >
-        Primary
-      </Button>
-      <Button
-        onClick={() => {
-          console.log("Hello");
-        }}
-        variant={"secondary"}
-      >
-        Secondary
-      </Button>
-    </CardContent>
-  </Card>
-);
+interface CardStoryProps {
+	fullWidth?: boolean
+}
+
+export const Default: Story<CardStoryProps> = ({ fullWidth }) => {
+	const buttonsWrapperClasses = cx({
+		buttonsWrapper: true,
+		buttonsWrapperFixedWidth: !fullWidth,
+	})
+	return (
+		<Card fullWidth={fullWidth}>
+			<CardImage url={AvatarTemplate} alt={'Default Avatar'} />
+			<CardContent>
+				<h1>Card name</h1>
+				<p>Card description goes here and it can be as long as it needs and so on.</p>
+				<div className={buttonsWrapperClasses}>
+					<Button
+						onClick={() => {
+							console.log('Hello')
+						}}
+						variant={'primary'}>
+						Primary Button
+					</Button>
+					<Button
+						onClick={() => {
+							console.log('Hello')
+						}}
+						variant={'secondary'}>
+						Secondary Button
+					</Button>
+				</div>
+			</CardContent>
+		</Card>
+	)
+}
 Default.args = {
-  fullWidth: false,
-};
+	fullWidth: false,
+}
--- src/surface-components/Card.tsx
+++ src/surface-components/Card.tsx
@@ -1,53 +1,31 @@
-import React, { PropsWithChildren } from "react";
-import styles from "./Card.module.css";
-import classNames from "classnames/bind";
+import React, { PropsWithChildren } from 'react'
+import styles from './Card.module.css'
+import classNames from 'classnames/bind'
 
-const cx = classNames.bind(styles);
+const cx = classNames.bind(styles)
 
 interface CardProps {
-  fullWidth?: boolean;
+	fullWidth?: boolean
 }
 
 export const Card = ({ fullWidth, children }: PropsWithChildren<CardProps>) => {
-  const CardClasses = cx({
-    fullWidth: fullWidth,
-    parentWidth: !fullWidth,
-  });
+	const cardClasses = cx({
+		cardFullWidth: fullWidth,
+		cardFixedWidth: !fullWidth,
+	})
 
-  return <div className={CardClasses}>{children}</div>;
-};
+	return <div className={cardClasses}>{children}</div>
+}
 
 interface CardImageProps {
-  url: string;
-  alt: string;
-  fullWidth?: boolean;
+	url: string
+	alt: string
 }
 
-export const CardImage = ({ url, alt, fullWidth }: CardImageProps) => {
-  const cardImageClasses = cx({
-    fullWidth: fullWidth,
-    parentWidth: !fullWidth,
-  });
-  return (
-    <div>
-      {" "}
-      <img src={url} alt={alt} />
-    </div>
-  );
-};
-
-interface CardContentProps {
-  fullWidth?: boolean;
+export const CardImage = ({ url, alt }: PropsWithChildren<CardImageProps>) => {
+	return <img src={url} alt={alt} className={styles.cardImage} />
 }
 
-export const CardContent = ({
-  children,
-  fullWidth,
-}: PropsWithChildren<CardContentProps>) => {
-  const cardContentClasses = cx({
-    fullWidth: fullWidth,
-    parentWidth: !fullWidth,
-  });
-
-  return <div>{children}</div>;
-};
+export const CardContent = ({ children }: PropsWithChildren) => {
+	return <div className={styles.cardContent}>{children}</div>
+}
