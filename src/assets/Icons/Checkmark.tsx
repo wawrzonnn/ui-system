9b6df0c2dc20ede3201800d5ed9ef593293eaf8a
wawrzonnn <norbert.wawrzonek@gmail.com>
2023-04-27 19:49:32 +0200

    refactor: format code with prettier

--- src/assets/Icons/Checkmark.tsx
+++ src/assets/Icons/Checkmark.tsx
@@ -1,12 +1,18 @@
-import * as React from 'react'
+import * as React from "react";
 
 export const Checkmark = () => {
-	return (
-		<svg width='11' height='8' viewBox='0 0 11 8' fill='none' xmlns='http://www.w3.org/2000/svg'>
-			<path
-				d='M0.290661 4.51562L1.26012 3.52628L3.77575 6.0071L9.10032 0.702414L10.0847 1.69176L3.77575 7.97585L0.290661 4.51562Z'
-				fill='white'
-			/>
-		</svg>
-	)
-}
+  return (
+    <svg
+      width="11"
+      height="8"
+      viewBox="0 0 11 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.290661 4.51562L1.26012 3.52628L3.77575 6.0071L9.10032 0.702414L10.0847 1.69176L3.77575 7.97585L0.290661 4.51562Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
--- src/navigation/Stepper/Stepper.tsx
+++ src/navigation/Stepper/Stepper.tsx
@@ -1,44 +1,54 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import React, { useState, createContext, PropsWithChildren, useContext } from 'react'
-import styles from './Stepper.module.css'
-import classNames from 'classnames/bind'
-const cx = classNames.bind(styles)
+import React, {
+  useState,
+  createContext,
+  PropsWithChildren,
+  useContext,
+} from "react";
+import styles from "./Stepper.module.css";
+import classNames from "classnames/bind";
+const cx = classNames.bind(styles);
 
 interface StepperContextProps {
-	activeStep: number
-	setActiveStep: React.Dispatch<React.SetStateAction<number>>
+  activeStep: number;
+  setActiveStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
 interface StepProps {
-	label: string
-	completed: boolean
-	disabled: boolean
-  index: number
+  label: string;
+  completed: boolean;
+  disabled: boolean;
+  index: number;
 }
 
 export const StepContext = createContext<StepperContextProps>({
-	activeStep: 0,
-	setActiveStep: () => {},
-})
+  activeStep: 0,
+  setActiveStep: () => {},
+});
 
-export const Stepper = ({ activeStep, children }: PropsWithChildren<StepperContextProps>) => {
-	const [activeStepState, setActiveStepState] = useState(activeStep)
+export const Stepper = ({
+  activeStep,
+  children,
+}: PropsWithChildren<StepperContextProps>) => {
+  const [activeStepState, setActiveStepState] = useState(activeStep);
 
-	return (
-		<StepContext.Provider value={{ activeStep: activeStepState, setActiveStep: setActiveStepState }}>
-			<div className={styles.stepperWrapper}>{children}</div>
-		</StepContext.Provider>
-	)
-}
+  return (
+    <StepContext.Provider
+      value={{ activeStep: activeStepState, setActiveStep: setActiveStepState }}
+    >
+      <div className={styles.stepperWrapper}>{children}</div>
+    </StepContext.Provider>
+  );
+};
 
 export const Step = ({ children, index }: PropsWithChildren<StepProps>) => {
-	const { activeStep } = useContext(StepContext)
+  const { activeStep } = useContext(StepContext);
 
-	const stepperClasses = cx({
+  const stepperClasses = cx({
     stepDefault: true,
-		stepCompleted: activeStep > index,
-		stepDisabled: activeStep < index,
-		stepActive: activeStep === index
-	})
-	return <div className={stepperClasses}>{children}</div>
-}
+    stepCompleted: activeStep > index,
+    stepDisabled: activeStep < index,
+    stepActive: activeStep === index,
+  });
+  return <div className={stepperClasses}>{children}</div>;
+};
